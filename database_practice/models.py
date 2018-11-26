b213ad8a4b755035db49cc385312f2899686cfe5
Taohidul <taohidulii@gmail.com>
2018-11-26 21:26:21 +0600

    two migrations added

--- database_practice/models.py
+++ database_practice/models.py
@@ -58,6 +58,41 @@ class Membership(models.Model):
     date_joined = models.DateField()
     invite_reason = models.CharField(max_length=100)
 
+class Person1(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+    def baby_boomer_status(self):
+        import datetime
+        if self.birth_date<datetime.date(1945,8,1):
+            return "Pre-boomer"
+        elif self.birth_date>datetime.date(1965,1,1):
+            return "Baby boomer"
+        else:
+            return "Post-boomer"
+
+    @property
+    def full_name(self):
+        return "%s %s"%(self.first_name,self.last_name)
+    test_property = "Test"
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline  = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def save(self,*args,**kwargs):
+        self.name +="(name modified)"
+        super().save(*args,**kwargs)
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
 
 
 
+class Student(CommonInfo):
+    home_group = models.CharField(max_length=100)
